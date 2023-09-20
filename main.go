23928f0c2c63f97449c6f0f39a79a8fc3b8627d5
ry-watanabe <ry-watanabe@sios.com>
2023-09-20 11:35:29 +0900

    接続文を修正

--- main.go
+++ main.go
@@ -180,7 +180,7 @@ func connectWithConnector() (*sql.DB, error) {
 			return d.Dial(ctx, instanceConnectionName, opts...)
 		})
 
-	dbURI := fmt.Sprintf("%s:%s@cloudsqlconn(localhost:3306)/%s?parseTime=true&loc=Asia%2FTokyo",
+	dbURI := fmt.Sprintf("%s:%s@cloudsqlconn(localhost:3306)/%s?parseTime=true&loc=Asia/Tokyo",
 		dbUser, dbPwd, dbName)
 
 	dbPool, err := sql.Open("mysql", dbURI)
